d6792ede6435ddfbabb4d97d6d7ab4dee57de159
shamil-skm <shamil.skm56@gmail.com>
2025-02-23 14:20:57 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -12,14 +12,8 @@ def error_message_detail(error):
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
-        self.error_message=error_message_detail(error_message)
+        self.error_message=error_message_detail(error_detail)
 
     def __str__(self):
         return self.error_message
     
-if __name__ == "__main__":
-    try:
-       a=1/0
-    except Exception as e:
-        logging.info("Divide By zero")
-        raise CustomException(e)
